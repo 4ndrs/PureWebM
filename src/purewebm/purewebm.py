7fb285e6019b8057d7593ea838cceb278227e377
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-09-09 19:49:56 +0200

    Move main() and parse_argv() to __main__.py

--- src/purewebm/purewebm.py
+++ src/purewebm/purewebm.py
@@ -6,58 +6,13 @@
 import sys
 import os
 import re
-import time
 import pathlib
 import hashlib
-import argparse
 import subprocess  # nosec
 from types import SimpleNamespace
-from multiprocessing import Process, Event, Manager
 from multiprocessing.connection import Listener, Client
 
-from . import CONFIG_PATH, __version__
-
-
-def main():
-    """Main function"""
-    kwargs = parse_argv()
-
-    verify_config()
-    socket = CONFIG_PATH / pathlib.Path("PureWebM.socket")
-
-    if socket.exists():
-        send(kwargs, socket)
-        print("Encoding information sent to the main process")
-        sys.exit(os.EX_OK)
-
-    # Main process does not exist, starting a new queue
-    manager = Manager()
-    encoding_done = Event()
-
-    queue = manager.Namespace()
-    queue.items = manager.list()
-    queue.total_size = manager.Value(int, 0)
-    enqueue(queue, kwargs)
-
-    listen_process = Process(target=listen, args=(queue, socket))
-    encode_process = Process(target=encode, args=(queue, encoding_done))
-    listen_process.start()
-    encode_process.start()
-
-    try:
-        while True:
-            if encoding_done.is_set():
-                listen_process.terminate()
-                socket.unlink()
-                sys.exit(os.EX_OK)
-
-            time.sleep(0.2)
-
-    except KeyboardInterrupt:
-        print("\nStopping (ctrl + c received)", file=sys.stderr)
-        listen_process.terminate()
-        encode_process.terminate()
-        sys.exit(-1)
+from . import CONFIG_PATH
 
 
 def enqueue(queue, kwargs):
@@ -594,81 +549,14 @@ def get_seconds(timestamp):
     return round(seconds, 3)
 
 
-def parse_argv():
-    """Parses the command line arguments"""
-    parser = argparse.ArgumentParser(
-        description="Utility to encode quick webms with ffmpeg"
+def print_progress(message, progress, total_size):
+    """Prints the encoding progress with a customized message"""
+    clear_line = "\r\033[K"
+    print(
+        f"{clear_line}Encoding {progress} of {total_size.get()}: {message}",
+        end="",
+        flush=True,
     )
-    parser.add_argument(
-        "--version", "-v", action="version", version=f"PureWebM {__version__}"
-    )
-
-    parser.add_argument(
-        "input",
-        nargs="+",
-        help="the input file(s) to encode (NOTE: these are only for a single "
-        "output file; to encode different files run this program multiple "
-        "times, the files will be queued in the main process using a Unix "
-        "socket)",
-    )
-    parser.add_argument(
-        "--output",
-        "-o",
-        help="the output file, if not set, the filename will be generated "
-        "using the filename of the input file plus a short MD5 hash and saved "
-        f"in {pathlib.Path('~/Videos/PureWebM').expanduser()}",
-    )
-
-    parser.add_argument(
-        "--encoder",
-        "-e",
-        default="libvpx-vp9",
-        help="the encoder to use (default is libvpx-vp9)",
-    )
-    parser.add_argument(
-        "--start_time",
-        "-ss",
-        help="the start time offset (same as ffmpeg's -ss)",
-    )
-    parser.add_argument(
-        "--stop_time",
-        "-to",
-        help="the stop time (same as ffmpeg's -to)",
-    )
-    parser.add_argument(
-        "--lavfi",
-        "-lavfi",
-        help="the set of filters to pass to ffmpeg",
-    )
-    parser.add_argument(
-        "--size_limit",
-        "-sl",
-        default=3,
-        type=float,
-        help="the size limit of the output file in megabytes, use 0 for no "
-        "limit (default is 3)",
-    )
-    parser.add_argument(
-        "--crf", "-crf", default="24", help="the crf to use (default is 24)"
-    )
-    parser.add_argument(
-        "--extra_params",
-        "-ep",
-        help="the extra parameters to pass to ffmpeg, these will be appended "
-        "making it possible to override some defaults",
-    )
-
-    kwargs = vars(parser.parse_args())
-    if "http" in kwargs["input"][0]:
-        kwargs["input"] = [pathlib.Path(url) for url in kwargs["input"]]
-    else:
-        kwargs["input"] = [
-            pathlib.Path(path).absolute() for path in kwargs["input"]
-        ]
-    if kwargs["output"]:
-        kwargs["output"] = pathlib.Path(kwargs["output"]).absolute()
-
-    return kwargs
 
 
 def verify_config():
@@ -683,17 +571,3 @@ def verify_config():
                 file=sys.stderr,
             )
             sys.exit(os.EX_CANTCREAT)
-
-
-def print_progress(message, progress, total_size):
-    """Prints the encoding progress with a customized message"""
-    clear_line = "\r\033[K"
-    print(
-        f"{clear_line}Encoding {progress} of {total_size.get()}: {message}",
-        end="",
-        flush=True,
-    )
-
-
-if __name__ == "__main__":
-    main()
