c999b0c73b222775ee2bf69b958f88b2ecf5a18f
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-09-17 23:56:00 +0200

    Add print_progress tests

--- tests/console_test.py
+++ tests/console_test.py
@@ -19,3 +19,31 @@ def test_print_encoding(capsys):
     console._print_encoding(1, value)
     captured = capsys.readouterr()
     assert captured.out == f"{CLEAR_LINE}Encoding 1 of 5: "
+
+
+def test_print_progress_blue(capsys):
+    total_size = Manager().Value(int, 3)
+    console.print_progress("Processing", 2, total_size, color="blue")
+    captured = capsys.readouterr()
+    assert (
+        captured.out == f"{CLEAR_LINE}Encoding 2 of 3: "
+        f"{COLOR.blue}Processing{COLOR.endc}"
+    )
+
+
+def test_print_progress_green(capsys):
+    total_size = Manager().Value(int, 2)
+    console.print_progress("Done", 2, total_size, color="green")
+    captured = capsys.readouterr()
+    assert (
+        captured.out == f"{CLEAR_LINE}Encoding 2 of 2: "
+        f"{COLOR.green}Done{COLOR.endc}"
+    )
+
+
+def test_print_progress_invalid(capsys):
+    total_size = Manager().Value(int, 1)
+    console.print_progress("Processing", 1, total_size, color="invalid")
+    captured = capsys.readouterr()
+    assert captured.out == f"{CLEAR_LINE}Encoding 1 of 1: "
+    assert captured.err == f"{COLOR.red}Unimplemented color: invalid\n"
